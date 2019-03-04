1719499a30ac72a44cccd1d5a45129039e6d13bc
Akshay <akshay.dosi@cognizant.com>
2019-03-04 21:10:29 +0530

    - Configurations Added : PhotoApp, PhotoApp(QA), PhotoApp(Prod) - APIConfig : file updated to read the user defined keys from build settings and info.plist file

--- PhotoApp/Network/APIConfig.swift
+++ PhotoApp/Network/APIConfig.swift
@@ -1,7 +1,6 @@
 /*
  FileName : APIConfig.swift
- Description : This is a structure that is for the configuration part, currently defaults
-                to only one environment. Used via Schemes as well
+ Description : This is a structure that is for the configuration part
  Copyright : Copyright © 2019 Cognizant. All rights reserved.
  Created Date : 01/23/19
  =============================================================================
@@ -10,10 +9,15 @@
 import Foundation
 
 struct APIConfig {
-    //Base URL
-    static var DevelopmentURL: String = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
-
     static var BaseURL: String {
-        return DevelopmentURL
+        return endpoint
+    }
+    static var endpoint: String {
+            if let path = Bundle.main.path(forResource: "Info", ofType: "plist") {
+                if let dict = NSDictionary(contentsOfFile: path) {
+                    return dict[Constants.apiBaseURL] as? String ?? ""
+                }
+            }
+            return ""
     }
 }
